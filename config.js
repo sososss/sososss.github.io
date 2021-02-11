27e93a06ea44ffd185e118cc87bbd43e38068dd8
Devy <sdk00sdk@naver.com>
2021-02-11 18:49:43 +0900

    Update postList style
    Signed-off-by: Devy <sdk00sdk@naver.com>

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Site Metadata
   title: `Soso's Blog`,
   description: `소소한 개발 이야기를 다루는..`,
-  author: ``,
+  author: `Soso's`,
   language: 'ko-KR',
   siteUrl: 'https://sososss.github.io/'
 };
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import './layout.scss';
 
@@ -9,11 +10,31 @@ interface LayoutPropsType {
 const Layout = (props: LayoutPropsType) => {
   const { children } = props;
 
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          author
+          title
+          siteUrl
+        }
+      }
+    }
+  `);
+
+  const siteMetadata = data.site.siteMetadata;
+
   return (
     <>
       <div id="layout">
         <div id="content">
           <main>{children}</main>
+          <footer>
+            <span>{`@ ${new Date().getFullYear()} ${siteMetadata.author} | Theme by `}</span>
+            <a href={siteMetadata.siteUrl}>{siteMetadata.author}</a>
+            <span>{` | Built with `}</span>
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
         </div>
       </div>
     </>
